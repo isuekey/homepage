40deb978ae3622977565320851428cb1e913d7f2
liuhanru <isuekey@163.com>
2020-07-31 10:11:47 +0800

    getting start

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-document.body.onload(function(){
+import * as convert from 'layerconvert';
+const onload = function(){
   const container = document.getElementById('container');
   console.log('i got the door');
-});
+  console.log('my convert', convert);
+  let element = document.createElement('div');
+  document.body.appendChild(element);
+};
+const body = document.body;
+body.onload = onload;
+
+
