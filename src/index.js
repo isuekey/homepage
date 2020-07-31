93fc610f55cc1f80b05ffb65b8e1c600c6b77be4
liuhanru <isuekey@163.com>
2020-07-31 19:41:25 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import * as convert from 'layerconvert';
+import './assets/css/style.css';
+
 const onload = function(){
   const container = document.getElementById('container');
   console.log('i got the door');
   console.log('my convert', convert);
-  let element = document.createElement('div');
+  const element = document.createElement('div');
+  element.innerHTML = '<span>Hello webpack</span>';
+  element.classList.add('hello');
   document.body.appendChild(element);
 };
 const body = document.body;
